69f0ac94def0a21ce1b6e0f3d515c2f281b7495f
crhilditch20 <crhilditch20@gmail.com>
2016-11-07 16:50:03 +0000

    commit car class and spec complete

--- cars.rb
+++ cars.rb
@@ -3,12 +3,36 @@ class Car
   attr_reader :colour, :model, :fuel, :speed
 
 
-  def initialize(colour, model, fuel, speed)
+  def initialize(colour, model, fuel, speed, driver)
     @colour = colour
     @model = model
     @fuel = fuel
     @speed = speed
+    @driver = driver
+    @passengers = []
   end
+
+  def accelerate
+    @speed += 10
+    @fuel -= 5
+  end
+
+  def brake
+    @speed -= 10 if (@speed > 0)
+  end
+
+  def get_driver_name
+    return @driver.name
+  end
+
+  def pick_up_passenger(new_passenger)
+    @passengers.push(new_passenger)
+  end
+
+  def count_passengers
+    @passengers.count
+  end
+
 
 
 end
--- cars_spec.rb
+++ cars_spec.rb
@@ -1,12 +1,15 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('cars')
+require_relative('person')
 
-class TestCar< Minitest::Test
+class TestCar < Minitest::Test
 
 def setup
 
-  @car = Car.new("blue", "Fiat500", 100, 0)
+  @person = Person.new("Claire", 35)
+  @car = Car.new("blue", "Fiat500", 100, 0, @person)
+  @passenger = Person.new("Jo", 35)
 
 end
 
@@ -18,4 +21,24 @@ end
     assert_equal("Fiat500", @car.model)
   end
 
+  def test_accelerate
+    @car.accelerate()
+    assert_equal(10, @car.speed)
+    assert_equal(95, @car.fuel)
+  end
+
+  def test_brake
+    @car.brake()
+    assert_equal(0, @car.speed)
+  end
+
+  def test_driver_name
+    assert_equal("Claire", @car.get_driver_name)
+  end
+
+  def test_pick_up_passenger
+    @car.pick_up_passenger(@passenger)
+    result = @car.count_passengers()
+    assert_equal(1, result)
+  end
 end
